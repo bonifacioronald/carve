7bd166c0252084870f17dbf81d429aa3fb530ac4
Rex Lim <rexlim2003@gmail.com>
2023-03-05 23:56:16 +0800

    tracker

--- lib/screens/tracker_screen.dart
+++ lib/screens/tracker_screen.dart
@@ -1,45 +1,47 @@
+import 'package:carve_app/widgets/calendar_selection.dart';
 import 'package:flutter/material.dart';
 import '../models/colors.dart' as custom_colors;
 import 'package:table_calendar/table_calendar.dart';
 
 import '../models/colors.dart';
-import '../widgets/calendar_selection.dart';
-import '../widgets/calendar_selection.dart';
+
 
 class TrackerScreen extends StatefulWidget {
-  const TrackerScreen({super.key});
 
   @override
   State<TrackerScreen> createState() => _TrackerScreenState();
-}
 
+}
 class _TrackerScreenState extends State<TrackerScreen> {
-  DateTime today = DateTime.now();
-  
-  CalendarFormat _calendarFormat=CalendarFormat.week;
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
-      body: Container(
+    return Scaffold(
+        body: Container(
             width: double.infinity,
             height: double.infinity,
+            color:backgroundPurple,
             padding: EdgeInsets.only(
                 left: 20,
                 right: 20,
                 bottom: 20,
                 top: MediaQuery.of(context).padding.top + 20),
-            child:Column(
+            
+                
+              child:
+              Column(
+                crossAxisAlignment:CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CalendarSelection(),
-
-
-
-            ]
-            )
-          )
-        );
+                
+                children: [
+                
+                Text("Growth Tracker",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                SizedBox(height: 10),
+                CalendarSelection()
+              ])
+          ));
   }
 }
