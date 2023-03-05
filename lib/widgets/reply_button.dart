7e389002bc695841ff36f5a2864acd090115cb6d
Hans <andreantohanss@gmail.com>
2023-03-06 00:43:38 +0800

    changed reply

--- lib/widgets/reply_button.dart
+++ lib/widgets/reply_button.dart
@@ -6,29 +6,28 @@ class replyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 348,
-      height: 48,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Color(0XFF02084B).withOpacity(0.1),
-          hintText: "Well, I think...",
-          hintStyle: TextStyle(
-              color: Color(0XFF02084B).withOpacity(0.3),
-              fontSize: 16,
-              fontWeight: FontWeight.bold),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide:
-                  BorderSide(color: Color(0XFF02084B).withOpacity(0.2))),
-          focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Color(0XFF02084B))),
+        width: 348,
+        height: 48,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
         ),
-      ),
-    );
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0XFF02084B).withOpacity(0.1),
+            hintText: "Well, I think...",
+            hintStyle: TextStyle(
+                color: Color(0XFF02084B).withOpacity(0.3),
+                fontSize: 16,
+                fontWeight: FontWeight.bold),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide:
+                    BorderSide(color: Color(0XFF02084B).withOpacity(0.2))),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0XFF02084B))),
+          ),
+        ));
   }
 }
