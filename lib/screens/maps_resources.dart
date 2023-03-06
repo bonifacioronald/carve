4bc2da59dd43eae8afc944a44597068a430ad5b0
Hans <andreantohanss@gmail.com>
2023-03-06 17:02:26 +0800

    Storing API shit

--- lib/screens/maps_resources.dart
+++ lib/screens/maps_resources.dart
@@ -1,12 +1,15 @@
-import 'package:carve_app/widgets/maps.dart';
+import 'package:carve_app/widgets/resources_maps.dart';
 import 'package:flutter/material.dart';
 
 class mapsResources extends StatelessWidget {
+  static const routeName = '/food-bank-map';
+
   const mapsResources({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: ,
       body: Container(
         child: MapScreen(),
       )
