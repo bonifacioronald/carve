fe9518f494e97527ae36404f31afa60ddd46c7c1
Hans <andreantohanss@gmail.com>
2023-03-06 12:34:02 +0800

    Optimized the layouting of Forum

--- lib/widgets/forum_content.dart
+++ lib/widgets/forum_content.dart
@@ -1,10 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:carve_app/screens/create_forum_screen.dart';
 import 'package:carve_app/screens/forum_page_screen.dart';
 import 'package:flutter/material.dart';
+import '/models/colors.dart' as custom_colors;
 
 class ForumCard extends StatelessWidget {
-  // username make it dynamic based on the username
   String username;
   String time;
   String category;
@@ -20,129 +19,141 @@ class ForumCard extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(bottom: 16),
       child: Container(
-          width: 352,
-          height: 192,
+          width: double.infinity,
           decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 1,
-                    blurRadius: 1,
-                    offset: Offset(0, 3))
-              ]),
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 8, top: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    AutoSizeText(
-                      username,
-                      style: TextStyle(
-                          color: Color(0XFF000000).withOpacity(0.4),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600),
-                      overflow: TextOverflow.ellipsis,
-                      minFontSize: 16,
-                      maxFontSize: 16,
-                      maxLines: 1,
-                    ),
-                    Icon(Icons.circle_rounded,
-                        color: Color(0XFF000000).withOpacity(0.5), size: 4),
-                    Text(time,
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Container(
+            padding: EdgeInsets.only(left: 8, right: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+                  child: Row(
+                    children: [
+                      AutoSizeText(
+                        username,
                         style: TextStyle(
-                            color: Color(0XFF000000).withOpacity(0.4),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600),
-                        overflow: TextOverflow.ellipsis),
-                    SizedBox(width: 4),
-                    Container(
-                        width: 92,
-                        height: 24,
+                          color: Colors.grey.withOpacity(0.4),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        minFontSize: 16,
+                        maxFontSize: 16,
+                        maxLines: 1,
+                      ),
+                      SizedBox(width: 4),
+                      Icon(Icons.circle_rounded,
+                          color: Colors.grey.withOpacity(0.5), size: 4),
+                      SizedBox(width: 4),
+                      Text(
+                        time,
+                        style: TextStyle(
+                          color: Colors.grey.withOpacity(0.4),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 2),
                         decoration: BoxDecoration(
-                          color: Color(0XFF8E93FB).withOpacity(0.2),
+                          color: custom_colors.secondaryLightPurple
+                              .withOpacity(0.2),
                           borderRadius: BorderRadius.circular(8),
                         ),
                         child: Center(
-                          child: Text(category,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w600)),
-                        )),
-                    SizedBox(width: 8),
-                    Icon(Icons.bookmark_add_outlined,
-                        color: Color(0XFF5B5B5B5B).withOpacity(0.5), size: 28),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 8),
-                child: Text(title,
-                    style: TextStyle(
-                        color: Color(0XFF02084B),
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600)),
-              ),
-              Divider(
-                color: Color(0XFF02084B).withOpacity(0.1),
-                indent: 8,
-                thickness: 2,
-                endIndent: 12,
-              ),
-              Container(
-                  padding: EdgeInsets.only(left: 8),
-                  child: Row(
-                    children: [
-                      Icon(Icons.thumb_up_alt_outlined,
-                          color: Color(0XFF5B5B5B5B).withOpacity(0.5),
-                          size: 24),
-                      Text(like,
-                          style: TextStyle(
-                              color: Color(0XFF5B5B5B5B).withOpacity(0.5),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                      SizedBox(width: 16),
-                      Icon(Icons.comment_outlined,
-                          color: Color(0XFF5B5B5B5B).withOpacity(0.5),
-                          size: 24),
-                      SizedBox(width: 4),
-                      Text(comments,
-                          style: TextStyle(
-                              color: Color(0XFF5B5B5B5B).withOpacity(0.5),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                      SizedBox(width: 75),
-                      TextButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Color(0XFFFFFFFF)),
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => forumPage()));
-                        },
-                        child: Container(
-                          alignment: Alignment.bottomRight,
-                          child: Text("Read more >",
-                              style: TextStyle(
-                                  color: Color(0XFF02084B),
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600)),
+                          child: Text(
+                            category,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
                         ),
                       ),
+                      Expanded(child: SizedBox()),
+                      Icon(Icons.bookmark_add_outlined,
+                          color: Colors.grey.withOpacity(0.5), size: 28),
                     ],
-                  ))
-            ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      color: custom_colors.primaryDarkPurple,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: Color(0XFF02084B).withOpacity(0.1),
+                  indent: 8,
+                  thickness: 2,
+                  endIndent: 12,
+                ),
+                Container(
+                    padding: EdgeInsets.only(left: 12, top: 0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.thumb_up_alt_outlined,
+                            color: Color(0XFF5B5B5B5B).withOpacity(0.5),
+                            size: 24),
+                        Text(like,
+                            style: TextStyle(
+                                color: Color(0XFF5B5B5B5B).withOpacity(0.5),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600)),
+                        SizedBox(width: 16),
+                        Icon(Icons.comment_outlined,
+                            color: Color(0XFF5B5B5B5B).withOpacity(0.5),
+                            size: 24),
+                        SizedBox(width: 4),
+                        Text(comments,
+                            style: TextStyle(
+                                color: Color(0XFF5B5B5B5B).withOpacity(0.5),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600)),
+                        SizedBox(width: 75),
+                        TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Color(0XFFFFFFFF)),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => forumPage()));
+                          },
+                          child: Container(
+                            alignment: Alignment.bottomRight,
+                            child: Text("Read more >",
+                                style: TextStyle(
+                                    color: Color(0XFF02084B),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600)),
+                          ),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
           )),
     );
   }
-
-  createForums() {}
 }
