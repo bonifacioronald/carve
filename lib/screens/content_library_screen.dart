ff4bc93bd5c07dd45dffb1f2715aa40f270f068c
Dylan <eu.dylan@gmail.com>
2023-03-07 22:34:12 +0800

    Fixed content and 50% done for content library

--- lib/screens/content_library_screen.dart
+++ lib/screens/content_library_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import '../models/colors.dart' as custom_colors;
 
@@ -6,16 +7,129 @@ class ContentLibraryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Center(
-        child: Text(
-          'Content Library Screen',
-          style: TextStyle(
-              color: custom_colors.primaryDarkPurple,
-              fontSize: 20,
-              fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          height: 1430,
+          color: custom_colors.primaryDarkPurple,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top + 50,
+                    left: 30,
+                    right: 30),
+                child: Text(
+                  "What to Learn?",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.only(left: 30, right: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 81,
+                      height: 85,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  "lib/assets/images/Content_Library_Assets/content_library_pregnant_mother.png"),
+                              fit: BoxFit.cover)),
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 250,
+                          child: AutoSizeText(
+                            "This should be a variable that takes in the content title",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                            maxFontSize: 17,
+                            minFontSize: 15,
+                            maxLines: 2,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Container(
+                          width: 230,
+                          height: 10,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(40),
+                            child: LinearProgressIndicator(
+                              minHeight: 5,
+                              color: custom_colors.secondaryLightPurple,
+                              backgroundColor: Colors.white,
+                              value:
+                                  0.8, //THIS SHOULD BE A VARIABLE THAT CHANGES BASED ON THE USER PROGRESS
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          "% the above progress bar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                width: double.infinity,
+                height: 1191,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                    image: DecorationImage(
+                        image: AssetImage(
+                            "lib/assets/images/Content_Library_Assets/content_library_background.png"),
+                        fit: BoxFit.cover)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Recommendations for you",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: custom_colors.primaryDarkPurple),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.filter_alt_sharp,
+                          color: custom_colors.primaryDarkPurple,
+                          size: 21,
+                        )
+                      ],
+                    )
+
+                    
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
