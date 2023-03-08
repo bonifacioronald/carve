d3348763b5dd8760e4ee08dd5f14d9875dff8866
Hans <andreantohanss@gmail.com>
2023-03-08 22:51:35 +0800

    Added Function Reply to Firebase

--- lib/widgets/reply_button.dart
+++ lib/widgets/reply_button.dart
@@ -1,6 +1,24 @@
+import 'package:carve_app/models/forum_model.dart';
+import 'package:carve_app/models/user_model.dart';
+import 'package:carve_app/providers/user_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:carve_app/providers/forum_reply_provider.dart';
+import 'package:provider/provider.dart';
 
 class ReplyButton extends StatelessWidget {
+  Future<void>? createNewReply(
+      String authorName, String content, String forumId, DateTime replyId) {
+    FirebaseFirestore.instance.collection('forumReply').doc().set({
+      "authorName": authorName,
+      "content": content,
+      'forumId': forumId,
+      'replyId': replyId,
+      'publishedDate': DateTime.now(),
+    });
+  }
+
+  final TextEditingController _content = TextEditingController();
   ReplyButton({Key? key}) : super(key: key);
 
   Color kBorderColor = Color(0XFF02084B).withOpacity(0.2);
@@ -11,6 +29,14 @@ class ReplyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    UserModel currentUser =
+        Provider.of<UserProvider>(context, listen: false).userProviderData;
+    ForumModel displayedContent =
+        ModalRoute.of(context)!.settings.arguments as ForumModel;
+
+    var _provider = Provider.of<ForumReplyProvider>(context, listen: false);
+    _provider.selectDisplayedForumReplybyId(displayedContent.id);
+
     return Container(
       width: 348,
       height: 48,
@@ -19,9 +45,15 @@ class ReplyButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
       ),
       child: TextField(
+        controller: _content,
         textAlign: TextAlign.left,
         decoration: InputDecoration(
-          suffixIcon: Icon(Icons.send, color: kFocusBorderColor),
+          suffixIcon: GestureDetector(
+              onTap: () {
+                createNewReply(currentUser.name, _content.text,
+                    displayedContent.id, DateTime.now());
+              },
+              child: Icon(Icons.send, color: kFocusBorderColor)),
           contentPadding: EdgeInsets.only(left: 16, right: 16),
           fillColor: kHintFillColor,
           hintText: "Well, I think...",
