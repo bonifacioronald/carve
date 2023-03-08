cf8523711c83e10ce0b1047f985ec4b2f059d360
Dylan <eu.dylan@gmail.com>
2023-03-08 21:51:15 +0800

    more changes

--- lib/models/content_model.dart
+++ lib/models/content_model.dart
@@ -3,10 +3,13 @@ class ContentModel {
   String title;
   List<String> content;
   List<String> suitableCategories;
+  String thumbnailUrl;
 
   ContentModel(
       {required this.id,
       required this.title,
       required this.content,
-      required this.suitableCategories});
+      required this.suitableCategories,
+      required this.thumbnailUrl});
 }
+
